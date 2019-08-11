7ac8a4bb69e294c42df76121f555777e0c1e83bc
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-11 21:05:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,7 +17,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min = 0
+  min = array[0]
   counter = 0
   
   while counter < array.length do
@@ -26,4 +26,6 @@ def find_min_value(array)
     end
     counter += 1
   end
+  
+  min
 end
